9bf5200f1018b5ad5c2acce65e56fbed999bec34
doctry <jonathanyang475869@gmail.com>
2021-01-10 23:14:31 +0800

    finish server construction

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+import {connectMongo, Task} from './db';
+
+connectMongo();
+
 const PORT = 4000;
 
 const app = require("express")();
@@ -10,8 +14,13 @@ const options = {
 };
 const io = require("socket.io")(server, options);
 
+// This part is for server behavior
 io.on("connection", (socket) => {
   console.log("a user connectted");
+
+  socket.on("addTask", (task) => {
+      Task.insertMany(task);
+  })
 });
 
 server.listen(PORT, () => {
