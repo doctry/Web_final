f81ce5fbfed355d52d5c282280a13a825ebcbc4a
Andreschao1999 <b06901176@ntu.edu.tw>
2021-01-17 22:33:37 +0800

    add third website board

--- src/in/Main.js
+++ src/in/Main.js
@@ -5,16 +5,19 @@ import { Home_page, Schedule_page, Contact_page, Reservation_page } from "./Page
 import Navbar_in from "./components/Navbar_in"
 import {Board0, url_0} from "./components/home/Board0"
 import {Board1, url_1} from "./components/home/Board1"
+import {Board2, url_2} from "./components/home/Board2"
 
 import "./Main.css";
 
 function Main() {
     const [board0, setBoard0] = useState([]);
     const [board1, setBoard1] = useState([]);
+    const [board2, setBoard2] = useState([]);
 
 	useEffect(() => {
         Board0(setBoard0)
         Board1(setBoard1)
+        Board2(setBoard2)
     }, []);
     
     return (
@@ -26,8 +29,9 @@ function Main() {
                         exact path="/" 
                         render={props => (
                             <Home_page {...props} 
-                                board0={board0} url0={url_0} name0={"台大社團經費補助"}
-                                board1={board1} url1={url_1} name1={"台大社團資訊"}
+                                board0={board0} url0={url_0} name0={"臺大社團經費補助"}
+                                board1={board1} url1={url_1} name1={"臺大社團資訊"}
+                                board2={board2} url2={url_2} name2={"臺大學生活動管理處"}
                             />
                         )}
                     />
