f296c0b1ab61045d761df49e297cdc4311551bda
Jian-Han0504 <jh880507@gmail.com>
2021-01-20 11:14:33 +0800

    Home page link css

--- src/in/components/home/WebPage.js
+++ src/in/components/home/WebPage.js
@@ -1,15 +1,15 @@
 function WebPage(props) {
 	return (
-		<div>
+		<div className="WebPage">
 			<h1 className="WebPage_h1">前往: <a href={props.url} target="_blank" className="WebPage_a1">{props.name}</a></h1>
 			{props.board.length ?
 				<ul className="WebPage_ul">
 					{props.board.map((item, index) => {
 						return (
 							<li key={index} className="WebPage_li">
-								<h2 className="WebPage_h2" style={item.color}>{item.type}</h2>
-								<a href={item.href} target="_blank" className="WebPage_a2">{item.title}</a>
-								<h3 className="WebPage_h3">{item.date}</h3>
+								<h2 style={item.color}>{item.type}</h2>
+								<a href={item.href} target="_blank" >{item.title}</a>
+								<h3>{item.date}</h3>
 							</li>
 						)
 					})}
