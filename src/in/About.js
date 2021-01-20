2a57f9530a6bcad255e458d5716dcad720d9eaba
Jian-Han0504 <jh880507@gmail.com>
2021-01-20 13:41:08 +0800

    About us

--- src/in/About.js
+++ src/in/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Row, Col } from "antd"
 import "./About.css"
 import department from "./components/img/about_us.png"
 
@@ -6,12 +7,19 @@ class About extends Component {
     render () {
         return (
         <div className="about-us">
-            <section className="department">
+            <div className="department">
                 <img src={department} alt="department"/>
                 <p>我們是一群電機系的夥伴</p>
                 <p>在這裡共同打造 一個屬於自己的天地</p>
-                
-            </section>
+            </div>
+
+            <div className="author">
+                <div>
+                    Author 1
+                </div>
+                <div>Author 2</div>
+                <div>Author 3</div>
+            </div>
         </div>
         );
     }
