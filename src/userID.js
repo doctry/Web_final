85eca6b431a72ef8d19bfd30a033a903eb437b60
doctry <jonathanyang475869@gmail.com>
2021-01-20 18:22:08 +0800

    finish weblink and schedule

--- src/userID.js
+++ src/userID.js
@@ -1,8 +1,10 @@
-const userID = () => {
+const UserID = () => {
     const user_id = localStorage.getItem("account");
     console.log(user_id)
 
     return user_id;
 }
+
+const userID = UserID()
 
 export default userID;
